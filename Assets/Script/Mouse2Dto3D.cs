13044fac4ba2edee0cddd9916bfad3ab0d0c8572
Đường Minh Khang <88478891+nghiatao30@users.noreply.github.com>
2024-04-29 17:22:55 +0700

    add mouse aiming in 3D

---
+++ Assets/Script/Mouse2Dto3D.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mouse2Dto3D : MonoBehaviour
+{
+    public Camera mainCam;
+    public LayerMask mouseColliderLayerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = mainCam.ScreenPointToRay( Input.mousePosition );
+        if(Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            transform.position = hitInfo.point;
+        }
+        
+    }
+}
